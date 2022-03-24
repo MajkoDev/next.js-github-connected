bc17803ffcf75cee33e7e44c60d2ed22ffe8472e
Marian <hellomariandev@gmail.com>
2022-03-24 23:27:15 +0100

    layout and repository card

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,23 @@ export default function Home() {
         <title>Github in Next.js</title>
       </Head>
 
+      <main className={styles.main}>
+        <h1 className={styles.title}>Github in Next.js</h1>
+
+        <p className={styles.description}>
+          How to fetch github repositories for your next.js website
+        </p>
+
+        <div className={styles.grid}>
+          <a href='link' className={styles.card}>
+            <h2>Title of Repository &rarr;</h2>
+            <h4 className={styles.date}>Created at</h4>
+            <h4 className={styles.date}>Updated at</h4>
+            <p>Description of Repository</p>
+          </a>
+        </div>
+      </main>
+
       <footer className={styles.footer}>
         <a
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
